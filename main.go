08fd31349a6247552f3f218e084f6a40c3b5483f
Jayendra Parsai <jparsai@jparsai-thinkpadp1gen4i.remote.csb>
2024-05-27 15:36:53 +0530

    gitops-demo

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -14,7 +13,7 @@ import (
 )
 
 func main() {
-	fmt.Println("####### Running #######")
+	fmt.Println("Process started...")
 	ctx := context.Background()
 
 	k8sClient, err := GetClient()
@@ -23,28 +22,39 @@ func main() {
 	}
 
 	if os.Getenv("CLUSTER_ROLE") != "" {
-		fmt.Println("Creating Cluster Role")
+
+		fmt.Println("Creating ClusterRole.")
 		clusterRole := &rbacv1.ClusterRole{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test-gitops-cluster-role",
 			},
 		}
-		err = k8sClient.Create(ctx, clusterRole)
+
+		if err = k8sClient.Create(ctx, clusterRole); err != nil {
+			fmt.Println("Failed to create ClusterRole", err)
+			return
+		}
+
+		fmt.Println("Succesfully created the ClusterRole : ", clusterRole.Name)
+
 	} else {
+
 		fmt.Println("Creating Role")
+
 		role := &rbacv1.Role{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "test-gitops-role",
 				Namespace: os.Getenv("POD_NAMESPACE"),
 			},
 		}
-		err = k8sClient.Create(ctx, role)
-	}
 
-	if err != nil {
-		fmt.Println("Failed to create object", err)
+		if err = k8sClient.Create(ctx, role); err != nil {
+			fmt.Println("Failed to create Role", err)
+			return
+		}
+
+		fmt.Println("Succesfully created the Role: %s in namespace: %s", role.Name, role.Namespace)
 	}
-	time.Sleep(5 * time.Minute)
 }
 
 func GetClient() (client.Client, error) {
